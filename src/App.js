8eef78d5fed196cd09278a0d72e88ac3d0c02f4f
sohail.khan1 <sohail.khan1@cars24.com>
2023-03-08 21:44:05 +0530

    master : styling completed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,11 @@ function App() {
   return (
     <div className="pageContainer">
       {/* <div className="card"></div> */}
-
+      <div className="card">
+        <p>Enter Mobile Number With Country Code</p>
+        <input placeholder="eg : +91866XXXXXX"/>
+        <button>Open</button>
+      </div>
     </div>
   );
 }
