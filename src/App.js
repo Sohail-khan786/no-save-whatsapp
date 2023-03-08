f7b1f91f2d2babb80780352395db59b0e18bc139
sohail.khan1 <sohail.khan1@cars24.com>
2023-03-08 21:53:04 +0530

    master : functionality implemented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [phoneNumber, setPhoneNumber] = useState("")
+
+  const openNumberOnWhatsapp = (phone) => {
+  const whatsappUrl = `https://wa.me/${phone}`.trim().replace(/\s/g,'').replace(/-/g,'')
+  window.open(whatsappUrl,"_blank")
+  
+  }
+
   return (
     <div className="pageContainer">
       {/* <div className="card"></div> */}
       <div className="card">
         <p>Enter Mobile Number With Country Code</p>
-        <input placeholder="eg : +91866XXXXXX"/>
-        <button>Open</button>
+        <input 
+          placeholder="eg : +91866XXXXXX" 
+          value={phoneNumber} 
+          onChange={(e)=>{setPhoneNumber(e.target.value)}}
+        />
+        <button onClick={()=>{openNumberOnWhatsapp(phoneNumber)}}>Open</button>
       </div>
     </div>
   );
