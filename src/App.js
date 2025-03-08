103c2cb38e7283fa47af1f210195c985c6fcad60
Sohail <sohail.khan1@cars24.com>
2025-03-08 16:15:48 +0530

    CTA text has been updated

--- src/App.js
+++ src/App.js
@@ -5,21 +5,21 @@ function App() {
   const [phoneNumber, setPhoneNumber] = useState("")
 
   const openNumberOnWhatsapp = (phone) => {
-  const whatsappUrl = `https://wa.me/${phone}`.trim().replace(/\s/g,'').replace(/-/g,'')
-  window.open(whatsappUrl,"_blank")
-  
+    const whatsappUrl = `https://wa.me/${phone}`.trim().replace(/\s/g, '').replace(/-/g, '')
+    window.open(whatsappUrl, "_blank")
+
   }
 
   return (
     <div className="pageContainer">
       <div className="card">
         <p>Enter Mobile Number With Country Code</p>
-        <input 
-          placeholder="eg : +91866XXXXXX" 
-          value={phoneNumber} 
-          onChange={(e)=>{setPhoneNumber(e.target.value)}}
+        <input
+          placeholder="eg : +91866XXXXXX"
+          value={phoneNumber}
+          onChange={(e) => { setPhoneNumber(e.target.value) }}
         />
-        <button onClick={()=>{openNumberOnWhatsapp(phoneNumber)}}>Open on WhatsApp</button>
+        <button onClick={() => { openNumberOnWhatsapp(phoneNumber) }}>Open on WAA</button>
       </div>
     </div>
   );
